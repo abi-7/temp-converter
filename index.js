a2f88afb3355f5cbecd3207d28cfb12e29ba7b33
Abi <abigailf03@hotmail.ca>
2024-07-29 08:39:20 -0500

    Added functionality
    Can convert Celsius to fahrenheit by clicking either display

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-//This function will convert the temperature of C to F and vise versa
-function convertTemp(){
+const displayC = document.getElementById("displayC");
+const displayF = document.getElementById("displayF");
 
-}
+//This will convert the temperature of C to F and vise versa
+displayC.addEventListener("input", () => {
+  const cel = parseFloat(displayC.value);
+  if (!isNaN(cel)) {
+    const fah = ((cel * 1.8) + 32).toFixed(3);
+    displayF.value = fah;
+  } else {
+    displayF.value = "";
+  }
+});
+
+displayF.addEventListener("input", () => {
+    const fah = parseFloat(displayF.value);
+    if (!isNaN(fah)) {
+      const cel = ((fah - 32) / 1.8).toFixed(3);
+      displayC.value = cel;
+    } else {
+      displayC.value = "";
+    }
+  });
